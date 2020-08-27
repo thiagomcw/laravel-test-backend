004dbeb94c06b899dc0e1775987e22d2d45d2a29
Thiago Alves <talves.ads@gmail.com>
2020-08-26 23:04:47 -0300

    adicionando componente para o menu

--- resources/static/js/app.js
+++ resources/static/js/app.js
@@ -4,6 +4,7 @@ window.Vue = require('vue');
 
 require('@fortawesome/fontawesome-free/js/all.js');
 
+Vue.component('navbar', require('./components/Navbar.vue').default);
 Vue.component('delete-link', require('./components/DeleteLink.vue').default);
 Vue.component('flash-alert', require('./components/FlashAlert.vue').default);
 
---
+++ resources/static/js/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            mobileOpened: false
+        };
+    },
+
+    computed: {
+        optionsClass() {
+            return this.mobileOpened ? 'block' : 'hidden';
+        }
+    },
+
+    methods: {
+        mobileToggle() {
+            this.mobileOpened = !this.mobileOpened
+        }
+    }
+}
+</script>
